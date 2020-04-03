4227b6c221d063b268f44cbf995f9f36316b1996
Henrik Korhonen <hevemiko@student.jyu.fi>
2020-04-03 12:52:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+TJTA330
+=======
+
+Install (Linux)
+---------------
+
+`$ git clone https://github.com/HenrikKorhonen/TJTA330.git backend && cd backend`
+
+`$ python3 -m venv env`
+
+`$ source ./env/bin/activate`
+
+`$ pip install -r requirements.txt`
+
+`$ python3 app.py`
+
+On other OSes, the third step is likely to be different. See https://docs.python.org/3/library/venv.html.
---
+++ app.py
@@ -0,0 +1,98 @@
+import os
+
+from flask import Flask, jsonify, Response, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+
+POSTGRES_USER = os.environ["PG_USER"]
+POSTGRES_PW = os.environ["PG_PASS"]
+POSTGRES_URL = "206.189.28.120:5432"
+POSTGRES_DB = "testing"
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
+#Modified
+#from the official Flask tutorial github.com/pallets/flask
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI=DB_URL,
+        SQLALCHEMY_TRACK_MODIFICATIONS="FALSE"
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)  
+    except OSError:
+        pass
+
+    db = SQLAlchemy(app)
+    Base = automap_base()
+    Base.prepare(db.engine, reflect=True)
+    julkaisu = Base.classes.julkaisu
+    henkilo = Base.classes.henkilo
+
+    @app.route("/titles/<string:id>")
+    def title(id):
+        try:
+            result = db.session.query(julkaisu).filter_by(tconst=id).first()
+            
+            return jsonify(tconst=result.id,
+                   titletype=result.titletype,
+                   primarytitle=result.primarytitle,
+                   originaltitle=result.originaltitle,
+                   isadult=result.isadult,
+                   startyear=result.startyear,
+                   endyear=result.endyear,
+                   runtimeminutes=result.runtimeminutes,
+                   genres=result.genres.split(","))
+            
+            
+        except Exception as e:
+            return Response(e, 500)
+        
+
+    @app.route("/names/<string:id>")
+    def name(id):
+        try:
+            result = db.session.query(henkilo).filter_by(nconst=id).first()
+            return jsonify(nconst=result.id,
+                   primaryname=result.primaryname,
+                   birthyear=result.birthyear,
+                   deathyear=result.deathyear,
+                   primaryprofession=result.primaryprofession,
+                   knownfortitles=result.knownfortitles.split(","))
+
+        except Exception as e:
+            return Response(e, 500)
+
+    @app.route("/search")
+    def search():
+        try:
+            query = request.args.get('q')
+            print(query)
+            results = db.session.query(julkaisu).filter(julkaisu.primarytitle.like("%{q}%".format(q=query))).all()
+            return jsonify(results=[result.id for result in results])
+
+        except Exception as e:
+            return Response(e, 500)
+        
+
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
+    
